06ce16e2c800087ca0921580e2205b395fc238eb
greenDT <d.a.trenchev@gmail.com>
2021-12-28 23:24:29 +0200

    Cosmetic changes

--- public/javascript/refreshChatMessages.js
+++ public/javascript/refreshChatMessages.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
     setInterval(function () {
         $( "#here" ).load(window.location.href + " #here" );
+        $('html,body').animate({scrollTop: document.body.scrollHeight},"fast");
     }, 1000);
 });
 //window.scrollTo(0,document.querySelector(".here").scrollHeight);
 //window.scrollTo(0,document.body.scrollHeight);
-$('html,body').animate({scrollTop: document.body.scrollHeight},"fast");
+//$('html,body').animate({scrollTop: document.body.scrollHeight},"fast");
 
